05cb35c373d632151632ba6d105b65e60318e2b4
Muhammadosama007 <115362575+Muhammadosama007@users.noreply.github.com>
2024-06-15 15:34:58 +0500

    json.parse

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ tb.style.color = "lightgray";
 tb.style.textAlign = "center"
 // const h=`<h1> Student Registration Form</h1>`;
 
-const arr = [];
+let arr = JSON.parse(localStorage.getItem("studentRegInfo")) || [];
+renderDetails(arr);
 function add() {
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
@@ -25,131 +26,11 @@ function add() {
     }
     arr.push(studentReg);
     console.log(arr);
-    const tb = document.getElementById("tb");
 
+    localStorage.setItem("studentRegInfo",JSON.stringify(arr));
+   
 
-
-
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const td3 = document.createElement("td");
-    const td4 = document.createElement("td");
-    const td5 = document.createElement("td");
-    for (let i = 0; i < arr.length; i++) {
-
-        td.innerHTML = fname;
-        td1.innerHTML = lname;
-        td2.innerHTML = reg;
-        td3.innerHTML = sec;
-        td4.innerHTML = program;
-
-        tb.appendChild(tr);
-        tr.appendChild(td);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-
-
-
-    }
-
-
-    const update = document.createElement("button");
-    const del = document.createElement("button");
-    update.innerHTML = "Edit";
-    del.innerHTML = "Delete";
-
-    tr.appendChild(td5);
-    td5.appendChild(update);
-    td5.appendChild(del);
-
-
-    update.style.backgroundColor = "green";
-    del.style.backgroundColor = "red";
-
-    update.style.color = "lightgray";
-    del.style.color = "lightgray";
-
-    del.addEventListener("click", function () {
-        // const index = arr[i];
-        const index = Array.from(tr.parentNode.children).indexOf(tr);
-        arr.splice(index, 1);
-        tb.removeChild(tr);
-        console.log(arr);
-    });
-
-
-
-    update.addEventListener("click", function () {
-        const finp = document.getElementById("fname");
-        const linp = document.getElementById("lname");
-        const rinp = document.getElementById("reg");
-        const sinp = document.getElementById("sec");
-        const pinp = document.getElementById("program");
-
-        finp.value = td.textContent;
-        linp.value = td1.textContent;
-        rinp.value = td2.textContent;
-        sinp.value = td3.textContent;
-        pinp.value = td4.textContent;
-
-        const saveBtn = document.createElement("button");
-        saveBtn.innerHTML = "SaveEdit";
-        tr.appendChild(saveBtn);
-
-        saveBtn.addEventListener("click", function () {
-            td.textContent = finp.value;
-            td1.textContent = linp.value;
-            td2.textContent = rinp.value;
-            td3.textContent = sinp.value;
-            td4.textContent = pinp.value;
-
-            const index = Array.from(tr.parentNode.children).indexOf(tr);
-            arr[index].f_name = td.textContent;
-            arr[index].l_name = td1.textContent;
-            arr[index].s_reg = td2.textContent;
-            arr[index].s_sec = td3.textContent;
-            arr[index].s_pro = td4.textContent;
-
-            console.log(arr);
-            tr.removeChild(saveBtn);
-            // tr.appendChild(update);
-            // tr.appendChild(del);
-            document.getElementById("fname").value = "";
-            document.getElementById("lname").value = "";
-            document.getElementById("reg").value = "";
-            document.getElementById("sec").value = "";
-            document.getElementById("program").value = "";
-        });
-    });
-    // update.addEventListener("click", function () {
-    //     const finp = document.getElementById("fname");
-    //     const linp = document.getElementById("lname");
-    //     const rinp = document.getElementById("reg");
-    //     const sinp = document.getElementById("sec");
-    //     const pinp = document.getElementById("program");
-
-    //     finp.value = td.textContent;
-    //     linp.value = td1.textContent;
-    //     rinp.value = td2.textContent;
-    //     sinp.value = td3.textContent;
-    //     pinp.value = td4.textContent;
-
-    //     td.textContent = "";
-    //     td1.textContent = "";
-    //     td2.textContent = "";
-    //     td3.textContent = "";
-    //     td4.textContent = "";
-
-    //     update.style.display = "none";
-    //     del.style.display = "block";
-    // });
-
-
-
+    renderDetails(arr);
 
     document.getElementById("fname").value = "";
     document.getElementById("lname").value = "";
@@ -160,3 +41,134 @@ function add() {
     document.body.style.backgroundColor = "lightgray";
 
 }
+
+// const tb = document.getElementById("tb");
+
+function renderDetails(arr) {
+    const tb = document.getElementById("tb");
+    tb.innerHTML = "";
+    arr.forEach(osama => {
+        const tr = document.createElement("tr");
+
+        const td = document.createElement("td");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const td4 = document.createElement("td");
+        const td5 = document.createElement("td");
+
+        td.innerHTML = osama.f_name;
+        td1.innerHTML = osama.l_name;
+        td2.innerHTML = osama.s_reg;
+        td3.innerHTML = osama.s_sec;
+        td4.innerHTML = osama.s_pro;
+
+        tb.appendChild(tr);
+        tr.appendChild(td);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+
+
+        const update = document.createElement("button");
+        const del = document.createElement("button");
+        update.innerHTML = "Edit";
+        del.innerHTML = "Delete";
+
+        tr.appendChild(td5);
+        td5.appendChild(update);
+        td5.appendChild(del);
+
+
+        update.style.backgroundColor = "green";
+        update.style.margin="5px";
+        update.style.fontSize="20px";
+        del.style.backgroundColor = "red";
+        del.style.margin="5px"
+        del.style.fontSize="20px";
+
+
+        update.style.color = "lightgray";
+        del.style.color = "lightgray";
+
+        del.addEventListener("click", function () {
+            // const index = arr[i];
+            const index = Array.from(tr.parentNode.children).indexOf(tr);
+            arr.splice(index, 1);
+            tb.removeChild(tr);
+            localStorage.removeItem("studentRegInfo");
+            localStorage.setItem("studentRegInfo",JSON.stringify(arr));
+            console.log(arr);
+        });
+
+
+
+        update.addEventListener("click", function () {
+            const finp = document.getElementById("fname");
+            const linp = document.getElementById("lname");
+            const rinp = document.getElementById("reg");
+            const sinp = document.getElementById("sec");
+            const pinp = document.getElementById("program");
+
+            finp.value = td.textContent;
+            linp.value = td1.textContent;
+            rinp.value = td2.textContent;
+            sinp.value = td3.textContent;
+            pinp.value = td4.textContent;
+
+            const saveBtn = document.createElement("button");
+            saveBtn.innerHTML = "SaveEdit";
+            tr.appendChild(saveBtn);
+            saveBtn.style.backgroundColor="blue";
+            saveBtn.style.fontSize="20px";
+            saveBtn.style.margin="5px";
+
+            saveBtn.addEventListener("click", function () {
+                td.textContent = finp.value;
+                td1.textContent = linp.value;
+                td2.textContent = rinp.value;
+                td3.textContent = sinp.value;
+                td4.textContent = pinp.value;
+
+                const index = Array.from(tr.parentNode.children).indexOf(tr);
+                arr[index].f_name = td.textContent;
+                arr[index].l_name = td1.textContent;
+                arr[index].s_reg = td2.textContent;
+                arr[index].s_sec = td3.textContent;
+                arr[index].s_pro = td4.textContent;
+
+
+                localStorage.setItem("studentRegInfo",JSON.stringify(arr));
+                console.log(arr);
+                tr.removeChild(saveBtn);
+
+                document.getElementById("fname").value = "";
+                document.getElementById("lname").value = "";
+                document.getElementById("reg").value = "";
+                document.getElementById("sec").value = "";
+                document.getElementById("program").value = "";
+            });
+        });
+
+
+
+    });
+
+
+}
+
+const search = document.getElementById("search");
+
+search.addEventListener("input", function () {
+    const newSearch = search.value.toLowerCase();
+    const filteredNames = arr.filter(students =>
+        students.f_name.toLowerCase().includes(newSearch) ||
+        students.l_name.toLowerCase().includes(newSearch) ||
+        students.s_reg.toLowerCase().includes(newSearch) ||
+        students.s_sec.toLowerCase().includes(newSearch) ||
+        students.s_pro.toLowerCase().includes(newSearch)
+    )
+    console.log(filteredNames);
+    renderDetails(filteredNames);
+});
